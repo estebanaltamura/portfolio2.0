6d697095471eb38967ceab818657a4053378959c
Andres <esteban.altamura@gmail.com>
2024-06-25 11:54:02 +0200

    update

--- src/app/_noPages/components/ExperienceItem.tsx
+++ src/app/_noPages/components/ExperienceItem.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { IExperiences } from '@/JSONs/JSONprojects';
-import { Avatar, Box, Typography, useMediaQuery } from '@mui/material';
-import { useRouter } from 'next/navigation';
+import { Box, Typography } from '@mui/material';
+import Link from 'next/link';
 
 interface IExperienceItemProps {
   index: number;
@@ -21,84 +20,78 @@ const ExperienceItem: React.FC<IExperienceItemProps> = ({
   role,
   period,
 }) => {
-  const router = useRouter();
-  const isDesktop = useMediaQuery('(min-width:600px)');
-
-  const experienceDetailClickHandler = () => {
-    router.push(`/experience-detail/${id}`);
-  };
-
   return (
-    <Box
-      sx={{
-        position: 'relative',
-        display: 'flex',
-        width: '100%',
-        height: '68px',
-        marginTop: index > 0 ? '35px' : '0px',
-        cursor: 'pointer',
-      }}
-      onClick={experienceDetailClickHandler}
-    >
+    <Link href={`/experience-detail/${id}`}>
       <Box
         sx={{
-          width: '15px',
-          height: '15px',
-          borderRadius: '100px',
-          backgroundColor: '#6E65A7',
-          marginTop: '3px',
+          position: 'relative',
+          display: 'flex',
+          width: '100%',
+          height: '68px',
+          marginTop: index > 0 ? '35px' : '0px',
+          cursor: 'pointer',
         }}
-      ></Box>
-      {index < length - 1 && (
+      >
         <Box
           sx={{
-            position: 'absolute',
-            width: '1px',
-            height: '88px',
-            top: '18px',
-            left: '7px',
-            backgroundColor: '#BABEBE',
+            width: '15px',
+            height: '15px',
+            borderRadius: '100px',
+            backgroundColor: '#6E65A7',
+            marginTop: '3px',
           }}
         ></Box>
-      )}
+        {index < length - 1 && (
+          <Box
+            sx={{
+              position: 'absolute',
+              width: '1px',
+              height: '88px',
+              top: '18px',
+              left: '7px',
+              backgroundColor: '#BABEBE',
+            }}
+          ></Box>
+        )}
 
-      <Box
-        sx={{ display: 'flex', flexDirection: 'column', marginLeft: '17px' }}
-      >
-        <Typography
-          sx={{
-            lineHeight: '18px',
-            fontSize: '16px',
-            fontWeight: '700',
-            marginTop: '2px',
-          }}
+        <Box
+          sx={{ display: 'flex', flexDirection: 'column', marginLeft: '17px' }}
         >
-          {name}
-        </Typography>
+          <Typography
+            sx={{
+              lineHeight: '18px',
+              fontSize: '16px',
+              fontWeight: '700',
+              marginTop: '2px',
+            }}
+          >
+            {name}
+          </Typography>
 
-        <Typography
-          sx={{
-            lineHeight: '18px',
-            fontSize: '13px',
-            fontWeight: '300',
-            marginTop: '2px',
-          }}
-        >
-          {period}
-        </Typography>
+          <Typography
+            sx={{
+              lineHeight: '18px',
+              fontSize: '13px',
+              fontWeight: '300',
+              marginTop: '2px',
+            }}
+          >
+            {period}
+          </Typography>
 
-        <Typography
-          sx={{
-            lineHeight: '22px',
-            fontSize: '13px',
-            fontWeight: '400',
-            marginTop: '4px',
-          }}
-        >
-          {role}
-        </Typography>
+          <Typography
+            sx={{
+              lineHeight: '22px',
+              fontSize: '13px',
+              fontWeight: '400',
+              marginTop: '4px',
+            }}
+          >
+            {role}
+          </Typography>
+        </Box>
       </Box>
-    </Box>
+    </Link>
   );
 };
 
